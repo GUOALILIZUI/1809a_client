b9ab820fd605122df42d24d80ea0d695c98b2d3f
alili <2115108140@qq.com>
2019-05-16 01:26:13 +0000

    dd

--- app/Http/Controllers/Center/CenterController.php
+++ app/Http/Controllers/Center/CenterController.php
@@ -11,14 +11,15 @@ class CenterController extends Controller
     public function center(Request $request){
         $token=$_GET['token'];
         $id=$_GET['id'];
-        $url='http://lumen.1809a.com/center?token='.$token+'&id='.$id;
+
+        $url='http://lumen.1809a.com/center?token='.$token.'&id='.$id;
         $ch=curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_HEADER, 0);
 
         $cc=curl_exec($ch);
         $oo=curl_errno($ch);
-        var_dump($oo);
+        var_dump($cc);
     }
 
     public function a(){
